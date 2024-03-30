7684e6c5f507ef6f681e4ca3edf472e93a96f862
Kwaran98 <142907620+Kwaran98@users.noreply.github.com>
2024-03-30 09:50:56 +0800

    Update index.js

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,24 +1,2 @@
-import axios from 'axios';
 
-const URL = 'https://car-api2.p.rapidapi.com/api/models?sort=id&direction=asc&year=2020&verbose=yes'
-
-const options = {
-  headers: {
-    'X-RapidAPI-Key': 'edf06044a2msh7ae1f11e4ad8708p1e0e36jsnd8bd11804a58',
-    'X-RapidAPI-Host': 'car-api2.p.rapidapi.com'
-  }
-};
-
-
-export const getCarData = async () => {
-    try {
-        const { data: { data }} = await axios.get(URL, options);
-        
-        return data;
-    }
-
-    catch(error){
-        console.log(error)
-    }
-}
 
